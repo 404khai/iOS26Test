4047eea990a0590a7068f2f6e85730e30f01083f
Khai <omajeneoghenefega11@gmail.com>
2026-05-28 00:35:17 -0700

    feat: implement NativeTab with Search

--- ContentView.swift
+++ ContentView.swift
@@ -7,43 +7,12 @@
 
 import SwiftUI
 
-// Tab Items
-enum CustomTab: String, CaseIterable{
-    case home = "Home"
-    case library = "Library"
-    case history = "History"
-    case extensions = "Extensions"
-    
-    var symbol: String {
-        switch self {
-        case .home: return "house"
-        case .library: return "books.vertical"
-        case .history: return "clock.arrow.circlepath"
-        case .extensions: return "puzzlepiece.extension"
-        }
-    }
-    
-    var actionSymbol: String {
-        switch self {
-        case .home: return "house"
-        case .library: return "books.vertical"
-        case .history: return "clock.arrow.circlepath"
-        case .extensions: return "puzzlepiece.extension"
-        }
-    }
-    
-    var index: Int {
-        Self.allCases.firstIndex(of: self) ?? 0
-    }
-}
-
 struct ContentView: View {
     @Namespace private var animation
     @State private var showMenu: Bool = false
-    @State private var showShareOptions: Bool = false
-    @State private var activeTab: CustomTab = .home
     
     var body: some View {
+        
         NavigationStack {
             List{
                 
@@ -72,54 +41,65 @@ struct ContentView: View {
 
         }
         
-        VStack {
-            GlassEffectContainer(spacing: 10){
-                HStack (spacing: 10) {
-                    GeometryReader{
-                        CustomTabBar(size: $0.size, activeTab: $activeTab) { tab in
-                            VStack (spacing: 3) {
-                                Image(systemName: tab.symbol)
-                                    .font(.title3)
-                                
-                                Text(tab.rawValue)
-                                    .font(.system(size: 10))
-                                    .fontWeight(.medium)
-                            }
-                            .symbolVariant(.fill)
-                            .frame(maxWidth: .infinity)
-                        }
-                        .glassEffect(.regular.interactive(), in: .capsule)
+        Group {
+            if #available(iOS 26, *) {
+                NativeTabView()
+            } else {
+                NativeTabView()
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func NativeTabView() -> some View {
+        TabView{
+            Tab.init("Home", systemImage: "house.fill"){
+                NavigationStack {
+                    List{
+                        
                     }
-                }
-                
-                ZStack {
-                    ForEach(CustomTab.allCases, id: \.rawValue){
-                        tab in
-                        Image(systemName: tab.actionSymbol)
-                            .font(.system(size: 22, weight: .medium))
-                            .blurFade(activeTab == tab)
-                    }
-                    .frame(width: 55, height: 55)
-                    .glassEffect(.regular.interactive(), in: .capsule)
-                    .animation(.smooth(duration: 0.55, extraBounce: 0), value: activeTab)
+                    .navigationTitle("Keihatsu")
                 }
             }
-            .frame(height: 55)
+            
+            Tab.init("Library", systemImage: "books.vertical"){
+                NavigationStack {
+                    List{
+                        
+                    }
+                    .navigationTitle("Library")
+                }
+            }
+            
+            Tab.init("History", systemImage: "clock.arrow.circlepath"){
+                NavigationStack {
+                    List{
+                        
+                    }
+                    .navigationTitle("History")
+                }
+            }
+            
+            Tab.init("Plugins", systemImage: "puzzlepiece.extension"){
+                NavigationStack {
+                    List{
+                        
+                    }
+                    .navigationTitle("Plugins")
+                }
+            }
+            
+            Tab.init("Search", systemImage: "magnifyingglass", role: .search){
+                NavigationStack {
+                    List{
+                        
+                    }
+                    .navigationTitle("Search")
+                }
+            }
         }
-        .padding(.horizontal, 20)
-
     }
-}
-
-// Blur Fade In/Out
-extension View {
-    @ViewBuilder
-    func blurFade (_ status: Bool) -> some View {
-        self
-            .compositingGroup()
-            .blur(radius: status ? 0 : 10)
-            .opacity(status ? 1 : 0)
-    }
+    
 }
 
 #Preview {
---
+++ CustomTabBarContentView.swift
@@ -0,0 +1,129 @@
+//
+//  CustomTabBarContentView.swift
+//  iOS26Test
+//
+//  Created by admin on 5/28/26.
+//
+
+import SwiftUI
+
+// Tab Items
+enum CustomTab: String, CaseIterable{
+    case home = "Home"
+    case library = "Library"
+    case history = "History"
+    case extensions = "Extensions"
+    
+    var symbol: String {
+        switch self {
+        case .home: return "house"
+        case .library: return "books.vertical"
+        case .history: return "clock.arrow.circlepath"
+        case .extensions: return "puzzlepiece.extension"
+        }
+    }
+    
+    var actionSymbol: String {
+        switch self {
+        case .home: return "house"
+        case .library: return "books.vertical"
+        case .history: return "clock.arrow.circlepath"
+        case .extensions: return "puzzlepiece.extension"
+        }
+    }
+    
+    var index: Int {
+        Self.allCases.firstIndex(of: self) ?? 0
+    }
+}
+
+struct CustomTabBarContentView: View {
+    @Namespace private var animation
+    @State private var showMenu: Bool = false
+    @State private var showShareOptions: Bool = false
+    @State private var activeTab: CustomTab = .home
+    
+    var body: some View {
+        NavigationStack {
+            List{
+                
+            }
+            .navigationTitle("Keihatsu")
+//            .navigationSubtitle("SurfUIKit for AstridOS")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Notifications", systemImage: "bell") {
+                        
+                    }
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Account", systemImage: "person") {
+                        showMenu.toggle()
+                    }
+                }
+                .matchedTransitionSource(id: "Account", in: animation)
+                    
+            }
+            .sheet(isPresented: $showMenu) {
+                Text("Account Sheet")
+                    .navigationTransition(.zoom(sourceID: "Account", in: animation))
+            }
+
+        }
+        
+        VStack {
+            GlassEffectContainer(spacing: 10){
+                HStack (spacing: 10) {
+                    GeometryReader{
+                        CustomTabBar(size: $0.size, activeTab: $activeTab) { tab in
+                            VStack (spacing: 3) {
+                                Image(systemName: tab.symbol)
+                                    .font(.title3)
+                                
+                                Text(tab.rawValue)
+                                    .font(.system(size: 10))
+                                    .fontWeight(.medium)
+                            }
+                            .symbolVariant(.fill)
+                            .frame(maxWidth: .infinity)
+                        }
+                        .glassEffect(.regular.interactive(), in: .capsule)
+                    }
+                }
+                
+                ZStack {
+                    ForEach(CustomTab.allCases, id: \.rawValue){
+                        tab in
+                        Image(systemName: tab.actionSymbol)
+                            .font(.system(size: 22, weight: .medium))
+                            .blurFade(activeTab == tab)
+                    }
+                    .frame(width: 55, height: 55)
+                    .glassEffect(.regular.interactive(), in: .capsule)
+                    .animation(.smooth(duration: 0.55, extraBounce: 0), value: activeTab)
+                }
+            }
+            .frame(height: 55)
+        }
+        .padding(.horizontal, 20)
+
+    }
+}
+
+// Blur Fade In/Out
+extension View {
+    @ViewBuilder
+    func blurFade (_ status: Bool) -> some View {
+        self
+            .compositingGroup()
+            .blur(radius: status ? 0 : 10)
+            .opacity(status ? 1 : 0)
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+
