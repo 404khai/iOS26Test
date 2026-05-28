862daefbbcc8d394605ecbd686b720ab1ded091d
Khai <omajeneoghenefega11@gmail.com>
2026-05-27 20:35:15 -0700

    feat: impl toolbars

--- ContentView.swift
+++ ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     @Namespace private var animation
+    @State private var showMenu: Bool = false
+    @State private var showShareOptions: Bool = false
     
     var body: some View {
         NavigationStack {
@@ -19,21 +21,56 @@ struct ContentView: View {
             .navigationSubtitle("SurfUIKit for AstridOS")
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
-                    Button("Settings", systemImage: "bell") {
+                    Button("Notifications", systemImage: "bell") {
                         
                     }
                 }
                 
                 ToolbarItem(placement: .topBarTrailing) {
                     Button("Account", systemImage: "person") {
-                        
+                        showMenu.toggle()
+                    }
+                }
+                .matchedTransitionSource(id: "Account", in: animation)
+                    
+                ToolbarItem(placement: .bottomBar) {
+                    Button("Share", systemImage: "square.and.arrow.up"){
+                        showShareOptions.toggle()
+                    }
+                }
+                .matchedTransitionSource(id: "Share", in: animation)
+                    
+                ToolbarSpacer(.flexible, placement: .bottomBar)
+                    
+                ToolbarItem(placement: .bottomBar) {
+                    Button("Clipboard", systemImage: "paperclip"){
+                            
+                    }
+                }
+                    
+                ToolbarItem(placement: .bottomBar) {
+                    Button("Write", systemImage: "square.and.pencil"){
+                            
                     }
                 }
             }
+            .sheet(isPresented: $showMenu) {
+                Text("Account Sheet")
+                    .navigationTransition(.zoom(sourceID: "Account", in: animation))
+            }
+            .sheet(isPresented: $showShareOptions) {
+                Text("Share Options")
+                    .navigationTransition(.zoom(sourceID: "Share", in: animation))
+                    .presentationDetents([.height(350)])
+            }
+
         }
+
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
