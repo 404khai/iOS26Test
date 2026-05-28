dd37e95ff0b804a669c96bfdf89c7864759e2c3c
Khai <omajeneoghenefega11@gmail.com>
2026-05-28 05:57:42 -0700

    feat: add CustomCarousel component

--- ContentView.swift
+++ ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
     
     var body: some View {
         
+        
         Group {
             if #available(iOS 26, *) {
                 NativeTabView()
---
+++ Model/Image.swift
@@ -0,0 +1,15 @@
+//
+//  Image.swift
+//  iOS26Test
+//
+//  Created by admin on 5/28/26.
+//
+
+import SwiftUI
+
+struct ImageModel: Identifiable{
+    var id: UUID = .init()
+    var image: String
+}
+
+var images: [ImageModel] = (1...6).compactMap({ImageModel(image: "Image \($0)")})
