37f301a42444de8dc17c6b342d4a90fe2e9c02c5
Khai <omajeneoghenefega11@gmail.com>
2026-05-27 18:19:51 -0700

    feat: project init

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS26Test
+//
+//  Created by admin on 5/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS26TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS26TestApp.swift
+//  iOS26Test
+//
+//  Created by admin on 5/27/26.
+//
+
+import SwiftUI
+
+@main
+struct iOS26TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
